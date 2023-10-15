8e092ae37b3f3b652e7feda961509ffd7daf622a
Zhiyuan-Wei <120235952+Zhiyuan-Wei@users.noreply.github.com>
2023-10-15 15:20:37 -0400

    Update tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -9,7 +9,6 @@ from .models import Question
 
 
 class QuestionModelTests(TestCase):
-
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
